b5c5ba33924ff729897a9aa86ab922d489b297a6
Eunseo Kim <alan0221@naver.com>
2023-09-12 11:46:19 +0900

    Create BOJ_1463.cpp

---
+++ BOJ/Silver/BOJ_1463.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int main() {
+    cin >> n;
+    int dp[1000001];
+	dp[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0) dp[i] = min(dp[i],dp[i / 3] + 1);
+		if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+	}
+	cout << dp[n] << '\n';
+}
